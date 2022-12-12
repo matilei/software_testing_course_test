cf1476c2b4d732371dd4d3b7be92afc2448451ee
Matias Leinonen <matias.leinonen98@gmail.com>
2022-12-12 16:13:30 +0200

    Modify tests

--- tests/Chunk.test.js
+++ tests/Chunk.test.js
@@ -3,9 +3,10 @@ import chunk from "../src/chunk";
 test('properly splits items into arrays', () => {
     const newList = chunk(['Oriveden kauramaito', 'Petterin maalaisleipä', 'Maijan tomaatti', 'Pohjoisen jääsalaatti'], 2);
 
-    /* CHUNK NOT WORKING PROPERLY, BELOW IS PASSING FALSELY
-    expect(newList).toEqual([['Maijan tomaatti', 'Pohjoisen jääsalaatti'], undefined]);
+    /* CORRECT FUNCTIONALITY
+    expect(newList).toEqual([['Oriveden kauramaito', 'Petterin maalaisleipä'], ['Maijan tomaatti', 'Pohjoisen jääsalaatti']])
     */
 
-    expect(newList).toEqual([['Oriveden kauramaito', 'Petterin maalaisleipä'], ['Maijan tomaatti', 'Pohjoisen jääsalaatti']])
+    // FALSE FUNCTIONALITY
+    expect(newList).toEqual([['Maijan tomaatti', 'Pohjoisen jääsalaatti'], undefined]);
 })
