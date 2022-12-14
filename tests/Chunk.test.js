73de193030ea3e2157fa08a30d8894283c90ff77
Matias Leinonen <matias.leinonen98@gmail.com>
2022-12-14 17:21:49 +0200

    Fix tests to have assumed implementation

--- tests/Chunk.test.js
+++ tests/Chunk.test.js
@@ -3,10 +3,5 @@ import chunk from "../src/chunk";
 test('properly splits items into arrays', () => {
     const newList = chunk(['Oriveden kauramaito', 'Petterin maalaisleipä', 'Maijan tomaatti', 'Pohjoisen jääsalaatti'], 2);
 
-    /* CORRECT FUNCTIONALITY
-    expect(newList).toEqual([['Oriveden kauramaito', 'Petterin maalaisleipä'], ['Maijan tomaatti', 'Pohjoisen jääsalaatti']])
-    */
-
-    // FALSE FUNCTIONALITY
-    expect(newList).toEqual([['Maijan tomaatti', 'Pohjoisen jääsalaatti'], undefined]);
+    expect(newList).toEqual([['Oriveden kauramaito', 'Petterin maalaisleipä'], ['Maijan tomaatti', 'Pohjoisen jääsalaatti']]);
 })
--- tests/Compact.test.js
+++ tests/Compact.test.js
@@ -3,11 +3,5 @@ import compact from "../src/compact";
 test('properly removes falsey values', () => {
     const newList = compact(['', 'Pohjoisen jääsalaatti', false, 'Petterin maalaisleipä', null, 'Tampereen makkara', 0])
 
-    /* CORRECT FUNCTIONALITY
     expect(newList).toEqual(['Pohjoisen jääsalaatti', 'Petterin maalaisleipä', 'Tampereen makkara'])
-    expect(newList).not.toEqual(['', 'Pohjoisen jääsalaatti', false, 'Petterin maalaisleipä', null, 'Tampereen makkara', 0])
-    */
-
-    // FALSE FUNCTIONALITY
-    expect(newList).not.toEqual(['Pohjoisen jääsalaatti', 'Petterin maalaisleipä', 'Tampereen makkara'])
 })
